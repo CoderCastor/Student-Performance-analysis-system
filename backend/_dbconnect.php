aff8e036e50cd57f21195d193fa96af862742e3a
CoderCastor <tejasshinde935@gmail.com>
2024-07-28 16:52:23 +0530

    Home and Login Pages developed

---
+++ backend/_dbconnect.php
@@ -0,0 +1,15 @@
+<?php 
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "project";
+
+$conn = mysqli_connect($server,$username,$password,$database);
+
+if($conn){
+} else{
+    die("Error".mysqli_connect_error());
+}
+
+?>
